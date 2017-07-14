de7876aa5c4f7b8587600e370aca8a526adc7cb1
石磊 <383821949@qq.com>
2017-07-14 14:10:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Scrapy-Docker-Docs
---
+++ docs/spiders/DockerDocs.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from docs.items import DocsItem
+import html2text
+
+class DockerdocsSpider(CrawlSpider):
+    name = 'DockerDocs'
+    allowed_domains = ['47.52.73.177']
+    start_urls = ['http://47.52.73.177:8888/']
+    
+    rules = (
+        Rule(LinkExtractor(allow=('', )), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = DocsItem()
+        item['url'] = response.url
+        item['description'] = response.xpath('//section[@class="section"]/h1/text()').extract()
+        item['title'] = response.xpath('//title/text()').extract()
+        item['data'] = html2text.html2text(response.xpath('//div[@class="col-body"]').extract()[0])
+        return item
