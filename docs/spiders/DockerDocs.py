4fb2924b9281d694f417ebd722b33e8317413f2b
石磊 <383821949@qq.com>
2017-07-17 16:43:47 +0800

    add *

--- docs/spiders/DockerDocs.py
+++ docs/spiders/DockerDocs.py
@@ -3,7 +3,7 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from docs.items import DocsItem
-import html2text
+import html2text, re
 
 class DockerdocsSpider(CrawlSpider):
     name = 'DockerDocs'
@@ -11,13 +11,16 @@ class DockerdocsSpider(CrawlSpider):
     start_urls = ['http://47.52.73.177:8888/']
     
     rules = (
-        Rule(LinkExtractor(allow=('', )), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=('', ), deny=('#.*?', 'term*?', )), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         item = DocsItem()
         item['url'] = response.url
-        item['description'] = response.xpath('//section[@class="section"]/h1/text()').extract()
-        item['title'] = response.xpath('//title/text()').extract()
+        item['description'] = response.xpath('//section[@class="section"]/h1/text()').extract()[0]
+        item['title'] = response.xpath('//title/text()').extract()[0]
         item['data'] = html2text.html2text(response.xpath('//main[@class="col-content content"]').extract()[0])
+        if re.findall('\*\*.*?\*\*', item['data']):
+            item['data'] = re.split('\*\*.*?\*\*\s', item['data'])[1]
+        item['data'] = re.sub('#\s', "", item['data'])[:100] + "..."
         return item
