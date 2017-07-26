8f8cbdbffda183dd1c98060f09b0f1fa1d72bc70
石磊 <383821949@qq.com>
2017-07-26 17:51:53 +0800

    add *

--- request.py
+++ request.py
@@ -65,11 +65,12 @@ def request_fingerprint(request, include_headers=None):
         fp = hashlib.sha1()
         fp.update(to_bytes(canonicalize_url(request.url)))
         cache[include_headers] = fp.hexdigest()
-        if request.url:
+        if not request.url == None:
             URL = ELASTICSEARCH_SEARCH_SERVERS + ELASTICSEARCH_DATA_INDEX + "/" + ELASTICSEARCH_SHA_TYPE + "/" + "_search?q=" + "url:" + "\"" + request.url + "\"" + "&size=1"
             # print(URL)
             Session = requests.get(url=URL).content
             for i in json.loads(Session):
+                Url_link = {}
                 Url_link = i['hits']['hits']['_source']['url']
                 if not Url_link == request.url:
                     DATA['timestamp'] = datetime.datetime.now()
