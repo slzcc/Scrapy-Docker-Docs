4de16eeea0b817a42b35063f788f8f0b401160fe
石磊 <383821949@qq.com>
2017-07-27 10:28:44 +0800

    add *

--- dupefilter.py
+++ dupefilter.py
@@ -127,20 +127,19 @@ class RFPDupeFilter(BaseDupeFilter):
         fp = self.request_fingerprint(request)
         # This returns the number of values added, zero if already exists.
         added = self.server.sadd(self.key, fp)
+
         URL = ELASTICSEARCH_SEARCH_SERVERS + ELASTICSEARCH_DATA_INDEX + "/" + ELASTICSEARCH_SHA_TYPE + "/" + "_search?q=" + "url:" + "\"" + request.url + "\"" + "&size=100"
         Session = RQ.get(url=URL).content
-        print("SESSION",Session)
-        SearchNum = json.loads(Session)['hits']['total']
-        if SearchNum >= 1:
-            for i in json.loads(Session)['hits']['hits']:
-                Url_link = i['_source']['url']
-                if not Url_link == request.url:
-                    DATA['timestamp'] = datetime.datetime.now()
-                    DATA['url']       = request.url
-                    DATA['sha1']      = fp
-                    _es.index(index=ELASTICSEARCH_DATA_INDEX, doc_type=ELASTICSEARCH_SHA_TYPE, body=DATA)
-                    _es.indices.refresh(index=ELASTICSEARCH_DATA_INDEX)
+        for i in json.loads(Session)['hits']['hits']:
+            Url_link = i['_source']['url']
+            if not Url_link == request.url:
+                DATA['timestamp'] = datetime.datetime.now()
+                DATA['url']       = request.url
+                DATA['sha1']      = fp
+                _es.index(index=ELASTICSEARCH_DATA_INDEX, doc_type=ELASTICSEARCH_SHA_TYPE, body=DATA)
+                _es.indices.refresh(index=ELASTICSEARCH_DATA_INDEX)
         #     # print("URL :", request.url, "SHA1 :", fp)
+
         return added == 0
 
     def request_fingerprint(self, request):
